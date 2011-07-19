633e86155f6fe7765c4218d142fc9fbe044562f9
ktwbc <kris@xsilva.com>
2011-07-19 16:16:14 -0400

    Fixes GH-15 Release of 2.1.1

--- includes/xlsws/xlsws.inc.php
+++ includes/xlsws/xlsws.inc.php
@@ -25,7 +25,7 @@
  */
 
     // Versioning Information
-    define('XLSWS_VERSION', '2.1.1b');
+    define('XLSWS_VERSION', '2.1.1');
 
     // Define default values
     define('XLS_TRUNCATE_PUNCTUATIONS'     , ".!?:;,-");
