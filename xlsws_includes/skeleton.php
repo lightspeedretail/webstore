a64a093514a3b78ae62a4511b78c678cfe443b95
Kris Thom White <kris@xsilva.com>
2011-10-02 17:00:17 -0400

    Fixes GH-125 skeleton misc_components failing E_STRICT

--- xlsws_includes/skeleton.php
+++ xlsws_includes/skeleton.php
@@ -1004,13 +1004,14 @@ class xlsws_index extends QForm {
 		$this->misc_components['order_subtotal']->CssClass =
 			"cart_line_selltotal";
 
-		$this->misc_components['order_total']->Text =
-			_xls_currency($objCart->Total);
-		$this->misc_components['order_total']->CssClass =
-			"cart_line_selltotal";
 
-		if ($this->misc_components['order_total'] instanceof QControl)
+		if ($this->misc_components['order_total'] instanceof QControl) {
+			$this->misc_components['order_total']->Text =
+			_xls_currency($objCart->Total);
+			$this->misc_components['order_total']->CssClass =
+			"cart_line_selltotal";
 			$this->misc_components['order_total']->Refresh();
+		}
 	}
 
 	// Create widgets to display Shipping cost data
@@ -1027,6 +1028,8 @@ class xlsws_index extends QForm {
 
 	// Update widgets to display Shipping cost data
 	protected function update_shippingcost_display($objCart) {
+		if (!isset($this->misc_components['order_shipping_cost']))
+			return false;
 		if ((strpos($objCart->IdStr,"WO-") === false || $objCart->Status != "Awaiting Processing")
 			&& $_GET['xlspg'] != "checkout" && $objCart->ShippingSell==0)
 				$this->misc_components['order_shipping_cost']->Text =
