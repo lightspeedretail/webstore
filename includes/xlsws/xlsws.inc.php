56231ca0990b71f192c8812e2c889a07e067734c
Kris Thom White <kris@xsilva.com>
2012-03-12 14:22:44 -0400

    Changing to dev version number

--- includes/xlsws/xlsws.inc.php
+++ includes/xlsws/xlsws.inc.php
@@ -25,7 +25,7 @@
  */
 
 // Versioning Information
-define('XLSWS_VERSION', '2.1.6');
+define('XLSWS_VERSION', '2.2.0dev');
 
 // Define default values
 define('XLS_TRUNCATE_PUNCTUATIONS', ".!?:;,-");
