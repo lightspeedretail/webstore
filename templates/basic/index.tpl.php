ae7a842a7e33edcea1c9afe5b2c2f74b856a107a
Kris Thom White <kris@xsilva.com>
2012-05-04 17:07:07 -0400

    #179 Google Analytics

--- templates/basic/index.tpl.php
+++ templates/basic/index.tpl.php
@@ -188,8 +188,21 @@
 	</div>
 
 
-	<!-- place google analytics here -->
+	<? if (_xls_get_conf('GOOGLE_ANALYTICS','') != ''): ?>
+	<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?= _xls_get_conf('GOOGLE_ANALYTICS') ?>']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<?php endif; ?>
 
 	<?php $this->dummy_drag_drop->Render(); ?>
 	    
