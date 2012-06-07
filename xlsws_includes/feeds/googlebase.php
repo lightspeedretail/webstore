e3d531e8348d300f9ca9394662a38b9f29686910
Kris Thom White <kris@xsilva.com>
2012-06-07 16:49:14 -0400

    #272 Google Base

--- xlsws_includes/feeds/googlebase.php
+++ xlsws_includes/feeds/googlebase.php
@@ -21,22 +21,22 @@ echo '		<link>'._xls_site_url().'</link>'.chr(13);
 echo '		<description>'._xls_get_conf('STORE_DEFAULT_SLOGAN').'</description>'.chr(13);
 
 		     
-$arrProducts = _dbx("SELECT * FROM xlsws_product WHERE web=1 ".$strQueryAddl." ORDER BY rowid limit 1", "Query");
+$arrProducts = _dbx("SELECT * FROM xlsws_product WHERE web=1 ".$strQueryAddl." ORDER BY rowid", "Query");
 while ($objItem = $arrProducts->FetchObject()) {
 	$objProduct = Product::Load($objItem->rowid);
 	
 	$strGoogle = _xls_get_googlecategory($objProduct->Rowid);
 
 	$arrTaxGrids = $objProduct->GetTaxRateGrid();
-	
+	$arrTrail = Category::GetTrailByProductId($objProduct->Rowid,'names');
   echo '<item>'.chr(13);
-      echo chr(9)."<g:id>".$objProduct->Rowid."</g:id>".chr(13);
-       echo chr(9).'<title>'.strip_tags($objProduct->Name).'</title>'.chr(13);
-       echo chr(9).'<description><![CDATA['.$objProduct->Description.']]></description>'.chr(13);
-   	   echo chr(9).'<g:google_product_category>'.$strGoogle.'</g:google_product_category>'.chr(13);
-	   echo chr(9).'<g:product_type><![CDATA['.implode(" &gt; ",Category::GetTrailByProductId($objProduct->Rowid,'names')).']]></g:product_type>'.chr(13);
-       echo chr(9).'<link>'.$objProduct->Link.'</link>'.chr(13);
-	   echo chr(9).'<g:image_link>http://www.foryarnssake.com/store/'.$objProduct->Image.'</g:image_link>'.chr(13);
+		echo chr(9)."<g:id>".$objProduct->Rowid."</g:id>".chr(13);
+		echo chr(9).'<title><![CDATA['.strip_tags($objProduct->Name).']]></title>'.chr(13);
+		if ($objProduct->Description) echo chr(9).'<description><![CDATA['.$objProduct->Description.']]></description>'.chr(13);
+		if ($strGoogle) echo chr(9).'<g:google_product_category>'.$strGoogle.'</g:google_product_category>'.chr(13);
+		if ($arrTrail) echo chr(9).'<g:product_type><![CDATA['.implode(" &gt; ",$arrTrail).']]></g:product_type>'.chr(13);
+		echo chr(9).'<link>'.$objProduct->Link.'</link>'.chr(13);
+		echo chr(9).'<g:image_link>'._xls_site_url($objProduct->Image).'</g:image_link>'.chr(13);
 	   
 	   //$images = Images::LoadArrayByProductAsImage($objProduct->Rowid , QQ::Clause(QQ::OrderBy(QQN::Images()->Rowid)));
 		//print_r($images);
@@ -47,16 +47,15 @@ while ($objItem = $arrProducts->FetchObject()) {
 	   		echo chr(9).'<g:availability>in stock</g:availability>'.chr(13);
 	   	elseif ($intStockHandling==2 && $objProduct->Inventory<=0)
 	   		echo chr(9).'<g:availability>available for order</g:availability>'.chr(13);
-	    elseif ($intStockHandling==1 && $objProduct->Inventory()<=0)
+	    elseif ($intStockHandling==1 && $objProduct->Inventory<=0)
 	   		echo chr(9).'<g:availability>out of stock</g:availability>'.chr(13);
 	   
-	   echo chr(9).'<g:price>'.$objProduct->Price.'</g:price>'.chr(13);
-	   echo chr(9).'<g:brand>'.$objProduct->Family.'</g:brand>'.chr(13);
-	   echo chr(9).'<g:gtin>'.$objProduct->Upc.'</g:gtin>'.chr(13);
+		echo chr(9).'<g:price>'.$objProduct->Price.'</g:price>'.chr(13);
+		echo chr(9).'<g:brand>'.$objProduct->Family.'</g:brand>'.chr(13);
+		echo chr(9).'<g:gtin>'.$objProduct->Upc.'</g:gtin>'.chr(13);
 	   
-	   
-	   echo '<product_color>'.$objProduct->ProductColor.'</product_color>'.chr(13);
-	   echo '<product_size>'.$objProduct->ProductSize.'</product_size>'.chr(13);
+		echo '<product_color>'.$objProduct->ProductColor.'</product_color>'.chr(13);
+		echo '<product_size>'.$objProduct->ProductSize.'</product_size>'.chr(13);
 
 		if ($objProduct->FkProductMasterId>0)
 			echo '<item_group_id>'.$objProduct->FkProductMasterId.'</item_group_id>'.chr(13);
