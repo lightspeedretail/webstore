8323f339b4e56e0d5fd1a04ee9328068b014a87e
Kris Thom White <kris@xsilva.com>
2012-08-24 12:13:11 -0400

    Fixed #453 Debug code removed

--- templates/deluxe/index.tpl.php
+++ templates/deluxe/index.tpl.php
@@ -28,7 +28,7 @@
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= _xls_get_conf('LANG_CODE' , 'en') ?>" dir="ltr">
-	<head><script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>
+	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?= _xls_get_conf('ENCODING' , 'utf-8') ?>" />
 	<?php
 		$redirect = _xls_stack_pop('xls_meta_redirect');
