dfacebf0559df4752aebe35e0df890fe5f203bb1
Kris Thom White <kris@xsilva.com>
2012-09-18 11:34:11 -0400

    Fixes #492 og:image doubling up subdirectory

--- templates/deluxe/sharing.tpl.php
+++ templates/deluxe/sharing.tpl.php
@@ -26,16 +26,25 @@
  */
 ?>
 <div id="sharingtools">
-	<a href="http://pinterest.com/pin/create/button/?url=<? echo $this->prod->CanonicalUrl; ?>&media=<? echo _xls_site_dir().$this->prod->SmallImage; ?>&description=<? echo urlencode($this->prod->Name); ?>" class="pin-it-button" count-layout="none"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>
+	<a href="http://pinterest.com/pin/create/button/?url=<? echo $this->prod->CanonicalUrl; ?>&media=<? echo
+	_xls_site_dir($this->prod->SmallImage,true); ?>&description=<? echo urlencode($this->prod->Name); ?>"
+	   class="pin-it-button" count-layout="none"><img border="0" src="//assets.pinterest.com/images/PinExt.png"
+	                                                  title="Pin It"/></a>
 
-<div class="g-plusone" data-size="medium" data-annotation="none" data-width="50"></div>
-	<script>(function(d){
-		var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-		js = d.createElement('script'); js.id = id; js.async = true;
+	<div class="g-plusone" data-size="medium" data-annotation="none" data-width="50"></div>
+	<script>(function (d) {
+		var js, id = 'facebook-jssdk';
+		if (d.getElementById(id)) {
+			return;
+		}
+		js = d.createElement('script');
+		js.id = id;
+		js.async = true;
 		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
 		d.getElementsByTagName('head')[0].appendChild(js);
 	}(document));</script>
-	<div class="fb-like" data-href="<? echo $this->prod->CanonicalUrl; ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false" style="vertical-align:top;zoom:1;*display:inline"> </div>
+	<div class="fb-like" data-href="<? echo $this->prod->CanonicalUrl; ?>" data-send="false" data-layout="button_count"
+	     data-width="90" data-show-faces="false" style="vertical-align:top;zoom:1;*display:inline"></div>
 
 
 </div>
