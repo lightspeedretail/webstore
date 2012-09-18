ecd6c5a405c6d8f00d852ddd866be873e07e7adc
Kris Thom White <kris@xsilva.com>
2012-09-18 13:46:25 -0400

    Fixes #493 Slider not scaling down

--- templates/minimal/slider.tpl.php
+++ templates/minimal/slider.tpl.php
@@ -1,13 +1,14 @@
 <?php
 $slider = $this->sliderName;
 ?>
-<div class="eleven columns alpha products_slider products_slider_theme" id="<?php echo $this->$slider->ControlId; ?>_slider">
-	<p class="back"><a href="#" title="Back"></a></p>
+<div class="eleven columns alpha omega products_slider products_slider_theme" id="<?php echo $this->$slider->ControlId; ?>_slider">
 
-	<p class="next"><a href="#" title="Next"></a></p>
+	<div class="one column alpha"><p class=" back highzindex"><a href="#" title="Back"></a></p></div>
+	<div class="eight columns"><span class="label"><?php echo $this->$slider->sliderTitle; ?></span></div>
+	<div class="one column omega leftpadding"><p class="highzindex next"><a href="#" title="Next"></a></p></div>
+	<br clear="both">
 
-	<p><span class="label"><?php echo $this->$slider->sliderTitle; ?></span></p>
-	<ul>
+		<ul>
 		<?php foreach ($this->$slider->links as $prod): ?>
 		<li><a href="<?php echo $prod['link']; ?>"><img src="<?php echo $prod['image']; ?>"
 		                                                alt="<?php echo $prod['title']; ?>"/></a>
