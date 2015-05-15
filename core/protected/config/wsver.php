33f53048ae8e8c3e3c507fe6ee51b4cc96aa5976
Kevin Ottley <kevin.ottley@lightspeedpos.com>
2015-05-15 10:35:08 -0400

    3.2.8

--- core/protected/config/wsver.php
+++ core/protected/config/wsver.php
@@ -1,5 +1,5 @@
 <?php
 
-define('XLSWS_VERSION', '3.2.7.1');
-define('XLSWS_VERSIONBUILD', 300200701);
-define('XLSWS_BUILDDATE', 'webstore-2015-05-14-1917');
+define('XLSWS_VERSION', '3.2.8');
+define('XLSWS_VERSIONBUILD', 300200800);
+define('XLSWS_BUILDDATE', 'webstore-2015-05-15-1249');
